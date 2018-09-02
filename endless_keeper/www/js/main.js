ad8001baa31e3731cb0115a79ae66cd87d275bb8
Jherson Martelo <yesungpapu@gmail.com>
2018-09-02 15:40:16 -0500

    Segundo mapa casi completo, falta añadir colisiones y triggers

--- endless_keeper/www/js/main.js
+++ endless_keeper/www/js/main.js
@@ -53,7 +53,9 @@ var boot = {
         game.load.audio('musica',['img/musica.ogg']);
         // assets para el mapa
 		game.load.tilemap('map', 'img/Endless_keeper.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.tilemap('map2', 'img/DangerMap.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.image('tiles', 'img/warTileset_32x32.png');
+        game.load.image('tiles2', 'img/wall.png');
         // assets para el jugador
         game.load.image('playerRight', 'img/Keeper47.png');
         game.load.image('playerLeft', 'img/Keeper47_left.png');
@@ -89,7 +91,7 @@ var boot = {
 
 	}
 }
-var initGame={
+var level1={
 	
 	create:function() {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -107,12 +109,7 @@ var initGame={
         map.setCollision(5);
         map.setCollision(9);
         map.setCollision(7);
-        map.setCollision(10);
-        map.setCollision(11);
-        map.setCollision(12);
-        map.setCollision(14);
-        map.setCollision(13);
-        map.setCollision(15);
+        map.setCollisionBetween(10,15);
         map.setTileIndexCallback(7, this.killPlayer, this);
         map.setTileIndexCallback(15, this.killPlayer, this);
         map.setTileLocationCallback(206,6,2,2,this.endMap, this);
@@ -152,13 +149,10 @@ var initGame={
 
         cursors = game.input.keyboard.createCursorKeys();
 
-        // Add the VirtualGamepad plugin to the game
         this.gamepad = this.game.plugins.add(Phaser.Plugin.VirtualGamepad);
             
-        // Add a joystick to the game (only one is allowed right now)
         this.joystick = this.gamepad.addJoystick(102, 280,1, 'gamepad');
         
-        // Add a button to the game (only one is allowed right now)
         this.button = this.gamepad.addButton(560, 280,0.7, 'gamepad');
 
         this.startTime = new Date();
@@ -178,7 +172,204 @@ var initGame={
 
         p.body.velocity.x = 0;
 
-        if (this.button.isDown)
+        if (cursors.up.isDown)
+        {
+            jump+=0.1;
+            myHealthbar.setPercent(100-jump);
+
+            if(jump>=100){
+                goToGameOver();
+                return false
+            }
+
+            if (p.body.onFloor())
+            {
+                p.body.velocity.y = -190;
+            }
+        }
+
+        if (this.joystick.properties.left)
+        {
+            p.body.velocity.x = -120;
+            this.changeSide(true);
+        }
+        else if (this.joystick.properties.right)
+        {
+            p.body.velocity.x = 120;
+            this.changeSide(false);
+        }
+        this.winPlayer(p);
+		switch(numIntentos){
+            case 1:
+                head1.kill();
+                head2.kill();
+                break;
+            case 2: 
+                head1.kill();
+                break;
+            default:
+                break;
+        }
+        if(p.body.x > (103*32)){
+            check = true;
+        }
+        else{
+            check = false;
+        }
+    },
+    endMap: function(evt){
+        this.winPlayer;
+        goToVictory();
+    },
+    winPlayer: function(){
+        var res = bestTime.split(":");
+        var local = localStorage.getItem(gameOptions.localStorageName);
+        var localSplit = local.split(":");
+        if(typeof local != "undefined"){
+            if(parseInt(res[0])<=parseInt(localSplit[0])){
+                if((parseInt(res[1])<=parseInt(localSplit[1])) || ((parseInt(localSplit[0])==0) && (parseInt(localSplit[1])==0))){
+                    localStorage.setItem(gameOptions.localStorageName, bestTime);
+                }
+            }
+        }
+    },
+
+	killPlayer: function (sprite, tile){ 
+        numIntentos--;
+        if(numIntentos<=0 || jump>=100){
+            goToGameOver();
+        }
+        else{
+            if(check){
+                p.body.x= (104*32);
+                p.body.y=(4*32);
+                return false;
+            }
+            p.body.x= 32;
+            p.body.y=200;
+            jump=0;
+            return false;
+        }
+        
+    },
+
+    changeSide: function (bool){
+        if(bool){
+            p.loadTexture('playerLeft', 0);
+        }
+        else{
+            p.loadTexture('playerRight', 0);
+        }
+    },
+    
+    updateTimer: function(){
+        var currentTime = new Date();
+        var timeDifference = this.startTime.getTime() - currentTime.getTime();
+            //Time elapsed in seconds
+        this.timeElapsed = Math.abs(timeDifference / 1000);
+    
+        //Time remaining in seconds
+        var timeRemaining = this.timeElapsed;
+    
+        //Convert seconds into minutes and seconds
+        var minutes = Math.floor(timeRemaining / 60);
+        var seconds = Math.floor(timeRemaining) - (60 * minutes);
+    
+        //Display minutes, add a 0 to the start if less than 10
+        bestTime = (minutes < 10) ? "0" + minutes : minutes;
+    
+        //Display seconds, add a 0 to the start if less than 10
+        bestTime += (seconds < 10) ? ":0" + seconds : ":" + seconds;
+        
+        timeText.setText(bestTime);
+    }
+}
+
+var level2={
+	
+	create:function() {
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        game.stage.backgroundColor = '#000';
+        music=game.add.audio('musica');
+        music.volume -= 0.8;
+        music.play();
+        map2 = game.add.tilemap('map2');
+
+        map2.addTilesetImage('wall', 'tiles2');
+
+        map2.setCollision(53);
+        map2.setCollision(73);
+        map2.setCollision(71);
+        map2.setCollision(1);
+        map2.setCollision(3);
+        map2.setCollision(36);
+        // map.setCollisionBetween(10,15);
+        // map.setTileIndexCallback(7, this.killPlayer, this);
+        // map.setTileIndexCallback(15, this.killPlayer, this);
+        // map.setTileLocationCallback(206,6,2,2,this.endMap, this);
+        
+        layer = map2.createLayer('backgroundZone');
+
+        // layer.debug = true;
+
+        layer.resizeWorld();
+
+
+        p = game.add.sprite(32, 200, 'playerRight');
+        game.physics.enable(p);
+
+        game.physics.arcade.gravity.y = 350;
+
+        p.body.bounce.y = 0;
+        p.body.linearDamping = 1;
+        p.body.collideWorldBounds = true;
+        p.scale.setTo(0.8);
+
+        head1 = game.add.sprite(190,10,'KeeperHead1');
+        head1.fixedToCamera = true;
+        head2 = game.add.sprite(220,10,'KeeperHead2');
+        head2.fixedToCamera = true;
+        head3 = game.add.sprite(250,10,'KeeperHead3');
+        head3.fixedToCamera = true;
+
+
+        barConfig = {x:550, y:22, flipped:true};
+        myHealthbar = new HealthBar(game, barConfig);
+        myHealthbar.setFixedToCamera(true);
+        myHealthbar.setBarColor('#aaa');
+        game.camera.follow(p);
+
+        btnAtras = game.add.button(0,10,'botonAtras',Inicio,this);
+        btnAtras.fixedToCamera=true;
+        btnAtras.scale.setTo(0.8); 	
+
+        cursors = game.input.keyboard.createCursorKeys();
+
+        this.gamepad = this.game.plugins.add(Phaser.Plugin.VirtualGamepad);
+            
+        this.joystick = this.gamepad.addJoystick(102, 280,1, 'gamepad');
+        
+        this.button = this.gamepad.addButton(560, 280,0.7, 'gamepad');
+
+        this.startTime = new Date();
+        this.totalTime = 120;
+        this.timeElapsed = 0;
+
+        timeText = game.add.text(barConfig.x-200,14, "00:00", { font: "16px Arial", fill: "#fff", align: "center" });
+        timeText.fixedToCamera = true;  
+
+        game.time.events.loop(Phaser.Timer.SECOND, this.updateTimer, this);
+
+
+    },
+    
+	 update:function() {
+		game.physics.arcade.collide(p, layer);
+
+        p.body.velocity.x = 0;
+
+        if (cursors.up.isDown)
         {
             jump+=0.1;
             myHealthbar.setPercent(100-jump);
@@ -401,7 +592,7 @@ function Inicio(){
     game.state.start('Inicio');
 }
 function goToGame(){
-    game.state.start('InitGame');
+    game.state.start('level2');
 }
 
 function goToCreditos(){
@@ -422,7 +613,8 @@ function goToVictory(){
 
 
 game.state.add('Boot',boot);
-game.state.add('InitGame', initGame);
+game.state.add('level1', level1);
+game.state.add('level2', level2);
 game.state.add('Inicio', inicio);
 game.state.add('Carga', carga);
 game.state.add('Creditos', creditos);
